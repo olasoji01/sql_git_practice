f0f47c8b4ab8165d2fa14969caf443c4c79cca34
olasoji01 <olasojiolamipo@gmail.com>
2024-09-12 13:05:23 +0100

    DQL

--- sqlfile.sql
+++ sqlfile.sql
@@ -124,3 +124,88 @@ WHERE student_id IN (3,7)
 --DELETE OPEARATION
 DELETE FROM student
 WHERE student_id=7
+
+--SELECTING FIRST 10 ROWS OF ALL ATTRIBUTE
+SELECT TOP(10) * FROM [Sample - Superstore]
+
+--FETCHING SPECIFIED COLUMNS 
+SELECT ship_mode, customer_name 
+FROM [Sample - Superstore]
+
+
+--USING ALIAS AND CALCULATE THE UNIT PRICE OF EACH QUANTITY
+SELECT product_name, customer_name as name, (sales/Quantity) as unit_price
+FROM [Sample - Superstore]
+
+
+--USING WHERE CLAUSE WITH > OPEARTION
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE quantity > 4;
+
+--using the between opeartion
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE quantity BETWEEN 2 AND 4
+
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE quantity >=2 AND quantity <5
+
+
+--using where clause and like operator
+--start word Like
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE city LIKE 'A%' AND quantity < 4
+--end word like 
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE city LIKE '%on'
+--middle word like or in between 
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE city LIKE '%_m_%'
+
+
+--USING IN OPERATOR TO FIND THE TRANSACTION OF KEN BLACK, JOEL ESTON, RYAN CROWE
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE customer_name IN ('Ken Black','Joel Eston','Ryan Crowe')
+
+
+--using order clause ASC
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE customer_name IN ('Ken Black','Joel Eston','Ryan Crowe')
+ORDER BY Quantity 
+
+--using order clause DESC
+SELECT ship_mode, customer_name, product_name, city, sales, quantity 
+FROM [Sample - Superstore]
+WHERE customer_name IN ('Ken Black','Joel Eston','Ryan Crowe')
+ORDER BY Quantity DESC
+
+--using GROUP BY clause
+SELECT city FROM [Sample - Superstore]
+GROUP BY city
+
+--using GROUP BY clause
+SELECT city, count(*)as customer_count FROM [Sample - Superstore]
+GROUP BY city
+
+--using having HAVING clause
+SELECT city, count(*)as customer_count FROM [Sample - Superstore]
+GROUP BY city
+HAVING count(*)>100
+
+SELECT city, SUM(sales) AS total_sales
+FROM [Sample - Superstore]
+GROUP BY city
+ORDER BY Total_sales DESC
+
+
+SELECT city, SUM(sales) AS total_sales, max(Quantity) AS Max_Quantity
+FROM [Sample - Superstore]
+GROUP BY city
+ORDER BY Total_sales DESC
